90f67ba41dde67720a7586fe69fa961d0409c397
root <howmarad@gmail.com>
2022-09-27 06:16:38 -0700

    Dissolve the arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
-if len(sys.argv) is 1:
-    print(0, 'arguments.')
-else:
-    if len(sys.argv) is 2:
-        print(len(sys.argv) - 1, 'argument:')
-        print('{}:'.format(len(sys.argv) - 1), sys.argv[1])
-    else:
-        print(len(sys.argv) - 1, 'arguments:')
-        for i in range(1, len(sys.argv)):
-            print('{}:'.format(i), sys.argv[i])
+if __name__ == "__main__":
+    from sys import argv
+    l = len(argv)
+    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
+                                 "." if l == 1 else ":"))
+    for i, s in enumerate(argv):
+        if i > 0:
+            print("{:d}: {:s}".format(i, s))
