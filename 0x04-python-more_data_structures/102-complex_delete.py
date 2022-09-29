3236bb73d57e914390262d3d1a6ccae2b3302d98
root <howmarad@gmail.com>
2022-09-29 01:10:05 -0700

    Delete a key with ists value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
+def complex_delete(my_dict, value):
+    keys_to_del = []
+    for key in my_dict:
+        if my_dict[key] == value:
+            keys_to_del.append(key)
+    for key in keys_to_del:
+        del my_dict[key]
+    return my_dict
