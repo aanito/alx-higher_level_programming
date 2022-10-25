54b04e9e2c73898cbcb2527982c5257d212f3dc9
root <howmarad@gmail.com>
2022-10-25 15:03:28 -0700

    The json fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 """
-this script add all arguments to a python list and
-save them into a specific given file
+Contains the "save_to_json_file" function
 """
 
+import json
 
-from sys import argv
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-filename = "add_item.json"
-
-try:
-    j_list = load_from_json_file(filename)
-
-except:
-    j_list = []
-
-for arguments in argv[1:]:
-    j_list.append(arguments)
-
-save_to_json_file(j_list, filename)
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
