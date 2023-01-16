d200c4e21a01f4b9f2da3075cac661d64e48a6f0
root <howmarad@gmail.com>
2023-01-16 06:32:01 -0800

    Hotfix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#Bash script for runnig a request url 
-#only line with the length of th body of response to be included
+#Bash script for runnig a request url only line with the length of th body of response to be included
 
 curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
