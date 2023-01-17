2587340cb9554928dab626222ac2e691a88b3ed5
root <howmarad@gmail.com>
2023-01-17 12:27:38 -0800

    Hot fix once

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""script that fetches https://intranet.hbtn.io/status"""
+"""fetches https://intranet.hbtn.io/status using requests"""
 import requests
 
 
 if __name__ == "__main__":
+    """fetches https://intranet.hbtn.io/status using requests"""
     r = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type:', type(r.text))
-    print('\t- content:', r.text)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
