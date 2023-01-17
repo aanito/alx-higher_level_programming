8c88b0e7c78631a2f280349d2f7e7f13959f4c50
root <howmarad@gmail.com>
2023-01-17 12:04:25 -0800

    Hot fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""
-script that fetches https://intranet.hbtn.io/status
-"""
+"""fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
 if __name__ == "__main__":
-    import urllib.request as request
-    with request.urlopen('https://intranet.hbtn.io/status') as ul:
-        html = ul.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    """fetches https://intranet.hbtn.io/status"""
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        html_str = html.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html_str))
